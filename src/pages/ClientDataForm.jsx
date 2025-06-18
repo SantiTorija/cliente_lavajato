fe395ee58ca070f4f4560dc94dda8aa9fb5d1520
SantiTorija <torija.santiago@gmail.com>
2025-06-18 15:00:49 -0300

    Update ClientDataForm.jsx

--- src/pages/ClientDataForm.jsx
+++ src/pages/ClientDataForm.jsx
@@ -29,9 +29,7 @@ const ClientDataForm = () => {
   const [carTypeId, setCarTypeId] = useState(null);
 
   const handleAddCarType = (carTypeId) => {
-    const selectedCarType = carTypes.find(
-      (type) => type.id === parseInt(carTypeId)
-    );
+    const selectedCarType = carTypes.find((type) => type.id === carTypeId);
     if (selectedCarType) {
       setCarType(selectedCarType.name);
       setCarTypeId(selectedCarType.id);
